17ae98f14e24209d3e5d3d84b0f0e00ee564e92e
dilanorantes <3049018570116@ingenieria.usac.edu.gt>
2025-12-28 13:21:31 -0600

    style(201807484): comentario en listas

--- BackEnd/src/main/java/com/backend/lista/Listas.java
+++ BackEnd/src/main/java/com/backend/lista/Listas.java
@@ -13,5 +13,6 @@ public class Listas {
     public static ArrayList<Paquete> listaPaquetes = new ArrayList<>();
     public static ArrayList<Ruta> listaRutas = new ArrayList<>();
     public static ArrayList<Mensajero> listaMensajeros = new ArrayList<>();
+    //tendria que usar otra lista para solicitudes
     public static ArrayList<Solicitud> listaSolicitudes = new ArrayList<>();
 }
